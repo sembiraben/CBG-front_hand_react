fd8187b91b860d561712d90b752be534180afd30
Ben Sembira <40471413+sembiraben@users.noreply.github.com>
2019-01-25 15:20:36 +0200

    Add files via upload

---
+++ build/precache-manifest.74fa833d110bf837124ba2a0a1c274fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "941d8c2d029a8e497af5",
+    "url": "/static/js/main.941d8c2d.chunk.js"
+  },
+  {
+    "revision": "35841f3932ef364e21a9",
+    "url": "/static/js/1.35841f39.chunk.js"
+  },
+  {
+    "revision": "941d8c2d029a8e497af5",
+    "url": "/static/css/main.a3f7729f.chunk.css"
+  },
+  {
+    "revision": "3fe76405462e35382ccae5acf9bd0335",
+    "url": "/index.html"
+  }
+];
